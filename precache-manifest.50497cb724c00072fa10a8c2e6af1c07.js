9d321487c57407de92e292af8adede4e8fb47146
Kh-Ol-An <olegkhristenko@gmail.com>
2019-10-09 16:11:11 +0300

    Updates

---
+++ precache-manifest.50497cb724c00072fa10a8c2e6af1c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8bf618ffcc612cd9ce215e42646e42",
+    "url": "/pure-test/index.html"
+  },
+  {
+    "revision": "aae886aaaddfe7565573",
+    "url": "/pure-test/static/css/main.cc634e1b.chunk.css"
+  },
+  {
+    "revision": "cae0b6e7683e2e7f956c",
+    "url": "/pure-test/static/js/2.b08f49f3.chunk.js"
+  },
+  {
+    "revision": "aae886aaaddfe7565573",
+    "url": "/pure-test/static/js/main.399da483.chunk.js"
+  },
+  {
+    "revision": "8fe04ea4319249575892",
+    "url": "/pure-test/static/js/runtime-main.89e6fae6.js"
+  },
+  {
+    "revision": "138cad48ec315dac943b67a4b6fcde18",
+    "url": "/pure-test/static/media/heart.138cad48.svg"
+  }
+]);
